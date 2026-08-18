cac24897f3c82dc00db697c3194a3290aa60e3d0
karantrackship-hash <karantrackship@gmail.com>
2026-08-18 16:16:01 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -17,3 +17,11 @@ export default defineConfig({
     },
   },
 });
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/ego/', // Replace <your-repository-name> with your actual repo name
+})
